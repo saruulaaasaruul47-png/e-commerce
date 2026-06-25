df1f4d270c8509d9b139c5bf9e593bb639035bea
saruulaaasaruul47-png <saruulaaasaruul47@gmail.com>
2026-06-25 11:21:36 +0800

    axios hiile

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import HeaderMenu from '../components/header/header'
 import Hero from '../components/hero/hero'
 import CardList from '../components/cardlist/cardlist'
@@ -7,11 +9,22 @@ import CertificateSection from '../components/certificate'
 import Title from '../components/title/title'
 
 const Home = () => {
+    const [data, setData] = useState()
+    const apiUrl = import.meta.env.VITE_API_URL
+    useEffect(() => {
+        const testFetch = async () => {
+            let data = await axios.get(`${apiUrl}`)
+            setData(data.data.message)
+            return data
+        }
+        testFetch()
+    },[])
+
     return (
         <div className="home overflow-x-hidden bg-[#f9fcf7]">
             <HeaderMenu />
             <div className="h-16 w-full bg-white lg:h-[76px]"></div>
-
+            <p>{data}</p>
             <Hero />
 
             <section className="mx-auto flex w-full max-w-[1400px] flex-col gap-8 px-4 py-10 sm:px-6 lg:gap-10 lg:px-10 lg:py-14">
